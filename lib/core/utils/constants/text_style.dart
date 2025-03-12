3a3e17d3b57f674511e3e9cf58fac4531ac9a40f
Mohammed El Shora <riyadm2001@gmail.com>
2025-03-12 11:48:24 +0200

    add fonts to app

---
+++ lib/core/utils/constants/text_style.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+abstract final class CustomTextStyle {
+  const CustomTextStyle();
+  static const String inter = "Inter";
+  static const String plusJakartaSans = "PlusJakartaSans";
+
+  static const TextStyle interMediam8 = TextStyle(
+    fontFamily: inter,
+    fontSize: 8,
+    fontWeight: FontWeight.w500,
+    letterSpacing: 0,
+  );
+
+  static const TextStyle interBold14 = TextStyle(
+    fontFamily: inter,
+    fontSize: 8,
+    fontWeight: FontWeight.w700,
+    letterSpacing: 0,
+  );
+  static const TextStyle plusMediam14 = TextStyle(
+    fontFamily: plusJakartaSans,
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    letterSpacing: -2,
+  );
+  static const TextStyle plusBold14 = TextStyle(
+    fontFamily: plusJakartaSans,
+    fontSize: 14,
+    fontWeight: FontWeight.w700,
+    letterSpacing: -2,
+  );
+}
